80fba5c8798446d25f0c96da80a1f63fadcc84f7
vsd121 <43914701+vsd121@users.noreply.github.com>
2025-04-11 08:55:01 +0530

    Add files via upload
    menu changes

---
+++ favorite.component.ts
@@ -0,0 +1,1 @@
+// Full content as shown previously for FavoriteComponent
---
+++ menu.actions.ts
@@ -0,0 +1,3 @@
+export const SET_INITIAL_DATA = '[Menu] Set Initial Data';
+export const TOGGLE_FAVORITE = '[Menu] Toggle Favorite';
+export const REQUEST_INITIAL_DATA = '[Menu] Request Initial Data';
